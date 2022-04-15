e8d884c1554e3012c95c6192f28c742307cdc51b
eudesgsantos <eudesgfilho@outlook.com>
2022-04-15 14:45:59 -0300

    Load balancer listener update

--- load_balancer/load_balancer.tf
+++ load_balancer/load_balancer.tf
@@ -8,29 +8,20 @@ resource "aws_lb" "alb" {
 
 resource "aws_lb_target_group" "tgA" {
   name        = "tgA"
-  port        = 80
+  port        = 3000
   protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = var.vpc
 }
 
-
 resource "aws_lb_listener" "albListener" {
   load_balancer_arn = aws_lb.alb.arn
   port              = "80"
   protocol          = "HTTP"
+
   default_action {
   type             = "forward"
   target_group_arn = aws_lb_target_group.tgA.arn
+  }
 }
 
-  # default_action {
-  #   type = "redirect"
-
-  #   redirect {
-  #     port        = "443"
-  #     protocol    = "HTTPS"
-  #     status_code = "HTTP_301"
-  #   }
-  # }
-}
