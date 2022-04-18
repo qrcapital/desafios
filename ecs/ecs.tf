70a6d2cb1b924f0a38857bfd628fce28baa354e1
eudesgsantos <eudesgfilho@outlook.com>
2022-04-18 10:20:35 -0300

    Added auto-scaling

--- ecs/ecs.tf
+++ ecs/ecs.tf
@@ -2,6 +2,7 @@
 #   name = "metabase/metabase"
 # }
 
+
 resource "aws_ecs_cluster" "ecs_cluster" {
   name = "ecs_cluster"
 
@@ -64,5 +65,45 @@ resource "aws_ecs_service" "ecs_service" {
     target_group_arn = var.tgA
     container_name   = "ecs_container"
     container_port   = 3000
+  }
+}
+
+resource "aws_appautoscaling_target" "dev_to_target" {
+  max_capacity = 4
+  min_capacity = 1
+  resource_id = "service/${var.ecs_cluster.name}/${var.ecs_service.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "dev_to_memory" {
+  name               = "dev-to-memory"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.dev_to_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.dev_to_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.dev_to_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+
+    target_value       = 80
+  }
+}
+
+resource "aws_appautoscaling_policy" "dev_to_cpu" {
+  name = "dev-to-cpu"
+  policy_type = "TargetTrackingScaling"
+  resource_id = aws_appautoscaling_target.dev_to_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.dev_to_target.scalable_dimension
+  service_namespace = aws_appautoscaling_target.dev_to_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+
+    target_value = 60
   }
 }
